9d37b9c23793b155e5aa5a5503cec2d1b0db3b64
Kanishk <150684142+Kanishk1420@users.noreply.github.com>
2025-07-08 13:41:33 +0530

    Disbale react hooks warnings

--- Frontend/src/pages/Auth/Login.jsx
+++ Frontend/src/pages/Auth/Login.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect, useState } from 'react';
 import { FaEyeSlash, FaRegEye } from "react-icons/fa";
 import { MdSunny } from "react-icons/md";
--- Frontend/src/pages/PostAction/EditPost.jsx
+++ Frontend/src/pages/PostAction/EditPost.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect, useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import { useGetPostByIdQuery, useUpdatePostMutation, useUploadFileMutation } from '../../api/post.js'
